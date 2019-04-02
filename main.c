b8a33969acf0b00a7ac1c81a24a2a4493c5bde9b
William Petrovsky <william.petrovsky@uon.edu.au>
2019-04-02 23:38:08 +0000

    Awful code

--- main.c
+++ main.c
@@ -1,9 +1,26 @@
-#include <std.io>
+#include <stdio.h>
 // Include project description
-
+/*
 //Function definitions need comments too
+// Encryption for Caesar cipher should take an encryption key between 0-25 
+//(distance of shift) and an input message, then produce an encrypted result.
+void encryptCaesar (char *str);
+// Decryption for Caesar cipher should take an encrypted message and decrypt it by testing
+// keys and checking for intelligble words
+void decryptCaesar (char *str);
 
+void encryptSub (char *str);
+
+void decryptSub(char *str);
+*/
 int main() {
-
-  return 0;
+    printf("This is a menu, select a, b, c, or d: ");
+    char c = scanf("%c", &c);
+    switch(c){
+        case 'a' : printf("a");
+        case 'b' : printf
+        
+    }
+    
+    return 0;
 }
