f63b433c5214a01bdd187600f8817461c5fc4d01
William Petrovsky <william.petrovsky@uon.edu.au>
2019-04-10 01:47:02 +0000

    Caesar cipher with error obeying key

--- main.c
+++ main.c
@@ -1,26 +1,46 @@
 #include <stdio.h>
 // Include project description
-/*
+char key = 3;
 //Function definitions need comments too
 // Encryption for Caesar cipher should take an encryption key between 0-25 
 //(distance of shift) and an input message, then produce an encrypted result.
-void encryptCaesar (char *str);
+char encryptCaesar (char e);
 // Decryption for Caesar cipher should take an encrypted message and decrypt it by testing
 // keys and checking for intelligble words
-void decryptCaesar (char *str);
+/*void decryptCaesar (char *str);
 
 void encryptSub (char *str);
 
-void decryptSub(char *str);
-*/
+void decryptSub(char *str);*/
+
 int main() {
-    printf("This is a menu, select a, b, c, or d: ");
-    char c = scanf("%c", &c);
-    switch(c){
-        case 'a' : printf("a");
-        case 'b' : printf
-        
-    }
+    FILE *input;
+    FILE *output;
     
+    input = fopen("input.txt", "r");
+    output = fopen("output.txt", "w");
+    
+    while (feof(input) == 0){
+            char e, f;
+            fscanf(input, "%c", &e);
+            f = encryptCaesar(e);  
+            fprintf(output, "%c", f);
+}
+
     return 0;
+}
+
+char encryptCaesar (char e){
+   
+            if(e >= 97 && e <= 122){ //make letter a capital
+                e = e - 32;
+            }
+            
+            if(e > 65 + key && e <= 90){
+                e = 65 + (26 - (e - 65) - key);
+            } else if (e >= 65 && e <= 65 + key) {
+                e = e + key;
+            }
+            return e;
+            
 }
